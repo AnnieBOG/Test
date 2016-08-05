d992f579f2be1f5f6c40f808404db21fe6c01021
Ana Margvelashvili <a.margvelashvili@bog.ge>
2016-08-05 10:59:18 +0400

    Initial commit

---
+++ hw1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                IntToString tmp = new IntToString();
+                Console.WriteLine(tmp.PrintString(n));
+            }
+        }
+    }
+}
